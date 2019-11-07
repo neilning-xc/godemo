5e09f465045895da489919ff425cb35e16aac652
Neil Ning <neil@CNC-Admins-MacBook-Air.local>
2019-11-07 18:10:49 +0800

    Parse token

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -1,9 +1,12 @@
 package token
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
@@ -22,4 +25,41 @@ func Sign(c Context) (string, error) {
 	})
 	tokenString, err := token.SignedString([]byte(secret))
 	return tokenString, err
+}
+
+func Parse(tokenString, jwtSecret string) (*Context, error) {
+	ctx := &Context{}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return ctx, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		ctx.Username = claims["username"].(string)
+		ctx.Email = claims["email"].(string)
+		return ctx, nil
+	}
+
+	return ctx, err
+}
+
+func ParseRequest(c *gin.Context) (*Context, error) {
+	header := c.Request.Header.Get("Authoirzation")
+
+	if len(header) == 0 {
+		return &Context{}, errors.New("No Authrization")
+	}
+
+	var tokenString string
+	fmt.Sscanf(header, "Bearer %s", &tokenString)
+	jwtSecret := viper.GetString("jwt_secret")
+
+	return Parse(tokenString, jwtSecret)
 }
